2b57a14cea3ecf7336efe0588e30fad5951ced77
bg117 - The OpenCode Team <76200573+bg117@users.noreply.github.com>
2021-12-18 19:12:02 +0800

    Add files via upload

---
+++ PictureToASCIIArt/GrayscaleConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureToASCIIArt;
+
+public static class GrayscaleConverter
+{
+    public static Bitmap ToGrayscale(this Bitmap input)
+    {
+        Bitmap grayscale = new(input, new Size(input.Width * 2, input.Height));
+
+        for (int x = 0; x < grayscale.Width; x++)
+        {
+            for (int y = 0; y < grayscale.Height; y++)
+            {
+                Color pixelColor = grayscale.GetPixel(x, y);
+                int gray = (int)((pixelColor.R * 0.3) + (pixelColor.G * 0.59) + (pixelColor.B * 0.11));
+                grayscale.SetPixel(x, y, Color.FromArgb(pixelColor.A, gray, gray, gray));
+            }
+        }
+
+        return grayscale;
+    }
+
+    public static int[,] ToIntRange(this Bitmap input)
+    {
+        int width = input.Width;
+        int height = input.Height;
+
+        int[,] array2d = new int[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Color cl = input.GetPixel(x, y);
+
+                int gray = (int)Convert.ChangeType((cl.R * 0.3) + (cl.G * 0.59) + (cl.B * 0.11), typeof(int));
+
+                array2d[x, y] = gray;
+            }
+        }
+
+        return array2d;
+    }
+}
---
+++ PictureToASCIIArt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using PictureToASCIIArt;
+
+if (!OperatingSystem.IsWindows())
+{
+    Console.WriteLine("This app doesn't work on operating systems other than Windows.");
+    return;
+}
+
+const string helpMessage =
+    "Flags:\n\n--render-file:<image-file> : Convert <image-file> to ASCII art\n\tExample usage: --render-file:image.jpg\n\n\tOptional\n\t--to:<file-path> : Write the ASCII art to <file-path>";
+
+if (0 < args.Length)
+{
+    string[] options = args[0].Split(':', 2);
+
+    switch (options[0])
+    {
+        case "--render-file":
+            bool exportToFile = false;
+            string? file = null;
+
+            if (1 < args.Length)
+            {
+                string[] exportOptions = args[1].Split(':', 2);
+                file = exportOptions[1];
+                exportToFile = true;
+            }
+
+            Bitmap bmp = new(options[1]);
+            int[,] intRange = bmp.ToGrayscale().ToIntRange();
+            string ascii = ASCII.FromIntRange(intRange);
+
+            if (!string.IsNullOrEmpty(ascii))
+            {
+                if (exportToFile && file != null)
+                    File.WriteAllText(file, ascii);
+                else
+                    Console.WriteLine(ascii);
+            }
+            
+            break;
+        default:
+            Console.WriteLine(helpMessage);
+            break;
+    }
+}
+else
+{
+    Console.WriteLine(helpMessage);
+}
